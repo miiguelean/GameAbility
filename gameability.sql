7f79a3ae8234c71c7602d481f8428ba02baabdf3
miigulean <mitwi26@gmail.com>
2024-03-22 11:03:33 -0500

    bd

--- gameability.sql
+++ gameability.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-03-2024 a las 21:11:42
+-- Tiempo de generación: 22-03-2024 a las 17:03:13
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
